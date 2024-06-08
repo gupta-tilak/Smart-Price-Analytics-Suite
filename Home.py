e1b7e058953d250fc398c45afbfc08d2dc19f427
Tilak Gupta <117450383+gupta-tilak@users.noreply.github.com>
2024-06-08 20:30:44 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="Home",
     page_icon="!!!"
 )
 
 st.title("Welcome to Smart Price Analytics Suite for properties of Gurgaon, India")
 
-st.sidebar.success("Select a demo above")
+st.sidebar.success("Select an option above")
